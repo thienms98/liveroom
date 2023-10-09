db483a077fc0d2f5d4f9d39e2bb6cd8806e9eadb
thienms98 <thienms98@gmail.com>
2023-10-09 14:19:12 +0700

    create home ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,177 @@
-import Image from 'next/image'
+'use client';
+
+import Image from 'next/image';
+import { useRef, useState } from 'react';
+import faker from '@faker-js/faker';
+
+interface User {
+  username: string;
+  image: string;
+  position?: {
+    top: number;
+    left: number;
+    zIndex: number;
+  };
+  room: string;
+}
+interface Room {
+  roomname: string;
+  position: {
+    top: number;
+    left: number;
+    zIndex: number;
+  };
+  size: {
+    width: number;
+    height: number;
+  };
+}
 
 export default function Home() {
+  const [rooms, setRooms] = useState<Room[]>([]);
+  const [users, setUsers] = useState<User[]>([]);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const [userSelect, setUserSelect] = useState<string>('');
+
+  const createRoom = () => {
+    setRooms((prev) => {
+      return [
+        ...prev,
+        {
+          roomname: 'room' + prev.length,
+          position: {
+            top: Math.floor(Math.random() * 200),
+            left: Math.floor(Math.random() * 500),
+            zIndex: 1,
+          },
+          size: {
+            width: 400,
+            height: 250,
+          },
+          users: [],
+        },
+      ];
+    });
+  };
+
+  const createUser = () => {
+    setUsers((prev) => [
+      ...prev,
+      {
+        username: 'user' + prev.length,
+        image: 'https://picsum.photos/id/' + prev.length + '/100',
+        room: '',
+      },
+    ]);
+  };
+  console.log(userSelect);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex flex-col bg-[#111] min-h-screen">
+      <div className="h-24 flex flex-row p-6 gap-3">
+        <div
+          className="h-full px-4 py-1 rounded-md border border-black bg-white text-black text-lg cursor-pointer hover:bg-white/95"
+          onClick={createRoom}
+        >
+          Create room
+        </div>
+        <div
+          className="h-full px-4 py-1 rounded-md border border-black bg-white text-black text-lg cursor-pointer hover:bg-white/95"
+          onClick={createUser}
+        >
+          Create user
         </div>
       </div>
+      <div
+        className="flex-1 bg-[#1e1e1e] rounded-lg m-3 overflow-auto relative flex flex-row flex-wrap gap-3 p-3"
+        ref={containerRef}
+        onClick={() => {
+          console.log('container');
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+          if (!userSelect) return;
+          const pos = users.findIndex((user) => user.username === userSelect);
+          if (pos === -1) return;
+          setUsers((prev) => {
+            const arr = [...prev];
+            arr[pos].room = '';
+            return arr;
+          });
+          setUserSelect('');
+        }}
+      >
+        {rooms.map((room, index) => (
+          <div
+            key={room.roomname}
+            className="border border-black bg-[#2e2e2e] rounded-lg transition-all flex flex-col"
+            style={{
+              // ...room.position,
+              ...room.size,
+            }}
+            onClick={(e) => {
+              e.stopPropagation();
+              console.log(room.roomname);
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+              if (!userSelect) return;
+              const pos = users.findIndex((user) => user.username === userSelect);
+              if (pos === -1) return;
+              setUsers((prev) => {
+                const arr = [...prev];
+                arr[pos].room = room.roomname;
+                return arr;
+              });
+              setUserSelect('');
+            }}
+            // draggable
+            // onMouseDown={(e) => {
+            //   setMovingRoom(index);
+            //   console.log(e);
+            // }}
+            // onMouseUp={(e) => setMovingRoom(-1)}
+          >
+            <span>{room.roomname}</span>
+            <div className="relative">
+              {users
+                .filter((user) => user.room && user.room === room.roomname)
+                .map(({ username, image }, index) => (
+                  <div
+                    className={`absolute w-12 h-12 rounded-full overflow-hidden object-contain cursor-pointer top-4 ${
+                      username === userSelect ? 'border-2 border-blue-500' : ''
+                    }`}
+                    style={{ backgroundImage: `url(${image})`, left: 16 + 24 * index }}
+                    key={index}
+                    title={username}
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      setUserSelect(username === userSelect ? '' : username);
+                    }}
+                  ></div>
+                ))}
+            </div>
+          </div>
+        ))}
+        <div className="flex flex-row flex-wrap gap-4">
+          {users
+            .filter((user) => !user.room)
+            .map(
+              ({ username, image, room }, index) =>
+                !room && (
+                  <div
+                    className={`w-12 h-12 rounded-full overflow-hidden object-contain cursor-pointer ${
+                      username === userSelect ? 'border-2 border-blue-500' : ''
+                    }`}
+                    style={{ backgroundImage: `url(${image})` }}
+                    key={index}
+                    title={username}
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      setUserSelect(username === userSelect ? '' : username);
+                    }}
+                  ></div>
+                ),
+            )}
+        </div>
       </div>
     </main>
-  )
+  );
 }
